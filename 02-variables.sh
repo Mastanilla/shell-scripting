ac380e06b37f4079c9d9eb2cf11f6fd8a27c9c72
hsjqajs.10@gmail.com <hsjqajs.10@gmail.com>
2026-02-07 08:32:48 -0600

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Please Enter your PIN Number"
-read PIN_NUMBER  #Here, PIN_NUMBER is Variable 
+read PIN_NUMBER  #Here, PIN_NUMBER is a Variable 
 
 echo "PIN Entered is: $PIN_NUMBER"
+
+echo "Please Enter your Password"
+read -s PASSWORD  #Here, PASSWORD is a Variable
+
+echo "Password is: $PASSWORD"
