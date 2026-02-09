9d3c7660c9fddaee261054a92237bedf53e0d9b5
hsjqajs.10@gmail.com <hsjqajs.10@gmail.com>
2026-02-09 07:55:17 -0600

    Date Variables

--- 03-env-variable.sh
+++ 03-env-variable.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "Course Name is: $COURSE"
+
+# Date Commands
+
+Date = $(date)
+
+STATRT_TIME=$(date +%s)
+
+sleep 10 &
+
+END_TIME=$(date +&s)
+
+TOTAL_TIME=$(END_TIME - START_TIME)
+
+echo "Script Executed Time in: $TOTAL_TIME Seconds"
+
+
